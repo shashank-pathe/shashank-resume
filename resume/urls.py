c05f63787a627e8eb596decae790f2b59a69cabd
shashank <shashankpathe@gmail.com>
2022-02-01 17:58:34 +0530

    helo

--- resume/urls.py
+++ resume/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf import url
+
 from app1 import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -25,7 +25,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homeview),
-    url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
