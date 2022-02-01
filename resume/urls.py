ee7be9a15644f10727ecddce8a2c6e9807673324
shashank <shashankpathe@gmail.com>
2022-02-01 17:55:39 +0530

    helo

--- resume/urls.py
+++ resume/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf.urls import url
+from django.conf import url
 from app1 import views
 from django.conf.urls.static import static
 from django.conf import settings
