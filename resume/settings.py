a19b7cc84643b3c6c22ffac5467e729bc94941ba
shashank <shashankpathe@gmail.com>
2022-02-01 18:01:39 +0530

    helo

--- resume/settings.py
+++ resume/settings.py
@@ -130,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
