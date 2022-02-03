011d299f1d188f273fed8a0ab5061c8948b76fe0
shashank <shashankpathe@gmail.com>
2022-02-03 18:44:21 +0530

    helo

--- resume/urls.py
+++ resume/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -26,7 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homeview),
     path('documents', views.DocumentsView),
-    path('d', views.Docs)
+    path('d', views.Docs),
+    url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    url(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 
     
 ]
