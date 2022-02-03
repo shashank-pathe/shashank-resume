76ff96464a7ec9a99d53f9796cd43a8a92f437c4
shashank <shashankpathe@gmail.com>
2022-02-03 15:51:58 +0530

    helo

--- app1/views.py
+++ app1/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import Mail
 from .forms import LoginForm
+from django.contrib import auth
 # Create your views hdef 
 
 def homeview(request):
     if request.method=="POST":
-        name = request.POST['name']
+        name = request.POST['name1']
         mail = request.POST['email']
         subject = request.POST['subject']
         message = request.POST['message']
@@ -15,12 +16,12 @@ def homeview(request):
         data.save()
         messages.success(request, 'Your message has been sent. Thank you!')
         return redirect('/')       
-    return render(request,'index.html')
+    return render(request,"index.html")         
+   
 
+def Documents(request):
 
-
-def loginview(request):
-    if request.method == 'POST':
+    if request.method=="POST":
         form=LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']  
@@ -30,7 +31,12 @@ def loginview(request):
             print(user)
             if user is not None:
                 auth.login(request,user)
-                return render(request,'loginview.html')
+                if user.is_superuser:
+                    messages.add_message(request, messages.INFO, 'logged in success')
+                    return redirect("admin/")
+                else:
+                    messages.add_message(request, messages.INFO, 'logged in success')  
+                    return render(request,'documents.html')
             else:
                 form= LoginForm()
                 context = {'form':form}
@@ -40,10 +46,16 @@ def loginview(request):
         else:
             form= LoginForm()
             context = {'form':form}     
-            return render(request,"base.html",context)
+            return render(request,"loginview.html",context)
     else:
-        form= LoginForm()
-        context = {'form':form}
-        messages.add_message(request, messages.WARNING, 'invalid credentials')     
+        form=LoginForm()
+        context = {'form':form} 
         return render(request,"loginview.html",context)
+    form=LoginForm()
+    context = {'form':form} 
+    return render(request,"loginview.html",context)
             
+def Docs(request):
+    form=LoginForm()
+    context = {'form':form} 
+    return render(request,'log.html',context)
--- resume/urls.py
+++ resume/urls.py
@@ -1,4 +1,4 @@
-"""resume URL Configuration
+"""shashank URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,20 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-
+from django.urls import path,include
 from app1 import views
-from django.conf.urls.static import static
-from django.conf import settings
-from django.views.static import serve
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homeview),
-    path('loginview', views.loginview),
-
-
-
+    path('documents', views.Documents),
+    path('d', views.Docs)
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
